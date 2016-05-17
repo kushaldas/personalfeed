b62b75552d64f47c9c24859153d02483c4ef0b2c
Kushal Das <mail@kushaldas.in>
2016-05-17 21:58:08 +0530

    Adds cleaned up HTML for each post

--- webfeed/pyfeed/__init__.py
+++ webfeed/pyfeed/__init__.py
@@ -1,6 +1,8 @@
+from bs4 import BeautifulSoup
 import redis
 import json
 import feedparser
+import sys
 
 # http://blog.yjl.im/2013/12/workaround-of-libxml2-unsupported.html
 feedparser.PREFERRED_XML_PARSERS.remove('drv_libxml2')
@@ -33,6 +35,25 @@ def safe_strings(s):
         return s.decode()
     return s
 
+def recreate_links(html_doc, url):
+    "Clean up the html for right urls"
+    if type(html_doc) == list:
+        html_doc = html_doc[0]['value']
+    res = []
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    for link in soup.find_all('a'):
+        text = link.get('href')
+        if text.startswith('/'):
+            res.append((text, "{0}{1}".format(url, text)))
+    for link in soup.find_all('img'):
+        text = link.get('src')
+        if text.startswith('/'):
+            res.append((text, "{0}{1}".format(url, text)))
+    for k,v in res:
+        html_doc = html_doc.replace(k, v)
+    return html_doc
+
 def startpoint():
     # first find all sites from the redis
 
@@ -44,6 +65,13 @@ def startpoint():
         fullposts = 'fullposts:{0}'.format(val)
         pturl = 'pt:{0}'.format(val)
         p = feedparser.parse(site)
+        # Save the link to the site so that we can replace
+        # the links where it is required
+        if 'feed' in p and 'link' in p['feed']:
+            url = p['feed']['link']
+            if url.endswith('/'):
+                url = url[:-1]
+            rdb.hset(pturl, 'link', url)
         posts = p.entries
         unread = rdb.hget(pturl, 'unread')
         if not unread:
@@ -62,6 +90,8 @@ def startpoint():
                     newd['content'] = safe_strings(p['content'])
                 else:
                     newd['content'] = safe_strings(p['summary'])
+                # Now clean it up
+                newd['content'] = recreate_links(newd['content'], url)
                 rdb.lpush(fullposts, json.dumps(newd))
                 unread += 1
         # Now all posts update, let us strip the list
