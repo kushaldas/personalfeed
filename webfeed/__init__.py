e51d829d366e9b95d973630c3dd8a3d4552cc632
Kushal Das <mail@kushaldas.in>
2016-05-18 20:03:05 +0530

    Removes redis, uses on memory dict

--- webfeed/__init__.py
+++ webfeed/__init__.py
@@ -1,12 +1,11 @@
 import flask
 import json
 import logging
-import redis
-import dbm
 from pprint import pprint
 import os
 import hashlib
 from flask import Flask, request
+from pyfeed import startpoint
 
 log = logging.getLogger()
 log.level = logging.DEBUG
@@ -15,25 +14,21 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
 #fh.setFormatter(formatter)
 #log.addHandler(fh)
 
-config = {
-            'host': 'localhost',
-            'port': 6379,
-            'db': 0,
-            'password': None,
-        }
 
-rdb = redis.Redis(config['host'], config['port'], config['db'],\
-                              config['password'])
+RDB = {'sites':{}, 'groups':{}}
 
 DBNAME = None
 
 class SiteDetails(object):
 
-    def __init__(self, url, value, unread, current=False):
-        self.url = url
-        self.value = value
+    def __init__(self, feed, hashvalue, unread=0, current=False):
+        self.url = ""
+        self.feed = feed
+        self.hashvalue = hashvalue
         self.unread = unread
         self.current = current
+        self.items = []
+        self.item_urls = {}
 
 class GroupDetails(object):
 
@@ -47,24 +42,14 @@ def get_all_site_details(name):
     Creates the all sites details for the navbar
     :return: A list of sites.
     """
+    global RDB
     result = []
-    sites = rdb.hgetall('sites')
-    groups = rdb.keys("group:*")
-    for g in groups:
-        g = g.decode()
-        ir = GroupDetails(g.split(':')[1])
-        gsites = rdb.lrange(g, 0, -1)
+    groups = RDB['groups']
+    for g, gsites in groups.items():
+        ir = GroupDetails(g)
         for k in gsites:
-            v = sites[k]
-            k = k.decode()
-            v = v.decode()
-            unread = rdb.hget('pt:{0}'.format(v), 'unread')
-            if not unread:
-                unread = b"0"
-            unread = unread.decode()
-            k = '/'.join(k.split('/')[:3])
-            data = SiteDetails(k, v, unread)
-            if v == name.strip():
+            data = RDB['sites'].get(k)
+            if data.hashvalue == name.strip():
                 data.current = True
             ir.items.append(data)
         result.append(ir)
@@ -73,43 +58,43 @@ def get_all_site_details(name):
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    sites = rdb.hgetall('sites')
-    sites = {k.decode():v.decode() for k,v in sites.items()}
+    global RDB
+    sites = RDB['sites']
+    sites = {v.feed:k for k,v in sites.items()}
     return flask.render_template('home.html', links=sites)
 
 @app.route('/read/<name>/')
 def read_a_site(name):
+    global RDB
     allsites = get_all_site_details(name)
-    fullposts = "fullposts:{0}".format(name)
-    pturl = "pt:{0}".format(name)
-    rdb.hset(pturl, 'unread', 0)
-    posts = rdb.lrange(fullposts, 0, -1)
-    posts = [json.loads(p.decode()) for p in posts]
+    pprint(allsites)
+    site = RDB['sites'][name]
+    site.unread = 0
+    RDB['sites'][name] = site
+    posts = site.items
     return flask.render_template('posts.html', posts=posts, allsites=allsites)
 
 @app.route('/addsites/<group>/',methods=['GET', 'POST'])
 def addsite(group):
+    global RDB
     if request.method == 'POST':
         url = request.form['url']
         url = url.strip()
         grp = request.form['group']
         grp = grp.strip()
         hash = hashlib.sha1(url.encode()).hexdigest()
-        rdb.hset('sites', url, hash)
-        rdb.rpush('group:{0}'.format(grp), url)
-        # Now let us save the data in dbm
-        db = dbm.open(DBNAME, 'c')
-        db[url] = grp
-        db.close()
+        site = SiteDetails(url, hash)
+        RDB['sites'][hash] = site
+        RDB['groups'].setdefault(grp, []).append(hash)
         return flask.render_template('addsites.html', group=group)
     else:
         return flask.render_template('addsites.html', group=group)
 
 @app.route('/update/')
 def update_sites():
-    rdb.lpush('updatejob', "hello")
-    sites = rdb.hgetall('sites')
-    return flask.render_template('home.html', links=sites)
+    global RDB
+    RDB = startpoint(RDB)
+    return flask.redirect(flask.url_for('hello_world'))
 
 
 
--- webfeed/pyfeed/__init__.py
+++ webfeed/pyfeed/__init__.py
@@ -1,39 +1,14 @@
 from bs4 import BeautifulSoup
-import redis
 import json
 import feedparser
 import sys
+from pprint import pprint
 
 # http://blog.yjl.im/2013/12/workaround-of-libxml2-unsupported.html
 feedparser.PREFERRED_XML_PARSERS.remove('drv_libxml2')
 
-config = {
-            'host': 'localhost',
-            'port': 6379,
-            'db': 0,
-            'password': None,
-        }
 
-rdb = redis.Redis(config['host'], config['port'], config['db'],\
-                              config['password'])
 
-def missing_link(key, link):
-    """
-
-    :param key: pt:URL a hash to find the current post URLS
-    :param link: the post URL we are looking
-    :return: True if it is missing, otherwise returns False
-    """
-    res = rdb.hget(key, link)
-    if not res:
-        return True
-    else:
-        return False
-
-def safe_strings(s):
-    if type(s) == bytes:
-        return s.decode()
-    return s
 
 def recreate_links(html_doc, url):
     "Clean up the html for right urls"
@@ -54,46 +29,37 @@ def recreate_links(html_doc, url):
         html_doc = html_doc.replace(k, v)
     return html_doc
 
-def startpoint():
+def startpoint(RDB):
     # first find all sites from the redis
 
-    sites = rdb.hkeys('sites')
-    for site in sites:
-        site = site.decode()
-        val = rdb.hget('sites', site)
-        val = val.decode()
-        fullposts = 'fullposts:{0}'.format(val)
-        pturl = 'pt:{0}'.format(val)
-        p = feedparser.parse(site)
+
+    for key, site in RDB['sites'].items():
+        p = feedparser.parse(site.feed)
         # Save the link to the site so that we can replace
         # the links where it is required
         if 'feed' in p and 'link' in p['feed']:
             url = p['feed']['link']
             if url.endswith('/'):
                 url = url[:-1]
-            rdb.hset(pturl, 'link', url)
+            site.url = url
         posts = p.entries
-        unread = rdb.hget(pturl, 'unread')
-        if not unread:
-            unread = 0
-        else:
-            unread = int(unread)
-        for p in posts[::-1]:
+        for p in posts:
             # First check if it is already in list.
             link = p['link']
-            if missing_link(pturl, link):
-                rdb.hset(pturl, link, True)
+            if link not in site.item_urls:
+                site.item_urls[link] = True
                 newd = {}
                 for k in ['summary','title','link','author','published']:
-                    newd[k] = safe_strings(p[k])
+                    newd[k] = p[k]
                 if 'content' in p:
-                    newd['content'] = safe_strings(p['content'])
+                    newd['content'] = p['content']
                 else:
-                    newd['content'] = safe_strings(p['summary'])
+                    newd['content'] = p['summary']
                 # Now clean it up
                 newd['content'] = recreate_links(newd['content'], url)
-                rdb.lpush(fullposts, json.dumps(newd))
-                unread += 1
+                site.items.append(newd)
+                site.unread += 1
         # Now all posts update, let us strip the list
-        rdb.ltrim(fullposts, 0, 20)
-        rdb.hset(pturl, 'unread', unread)
+        RDB[key] = site
+        pprint(RDB)
+    return RDB
